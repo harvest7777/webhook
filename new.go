c524e642b95db1688d00662c0ba38353cd98b528
harvest7777 <333harvest.moon333@gmail.com>
2026-08-20 20:17:23 -0700

    hello world

---
+++ new.go
@@ -0,0 +1,8 @@
+// its as if go stitches all these same-package files together
+// this is just a smipler way to organize codee
+package main
+import "fmt" 
+
+func hello() {
+    fmt.Println("hello")
+}
