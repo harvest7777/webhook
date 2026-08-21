212dbf1046f47f19ab49c37034f5cfd86e9134c5
harvest7777 <333harvest.moon333@gmail.com>
2026-08-21 02:41:32 -0700

    move files

--- new.go
+++
@@ -1,8 +0,0 @@
-// its as if go stitches all these same-package files together
-// this is just a smipler way to organize codee
-package main
-import "fmt" 
-
-func hello() {
-    fmt.Println("hello")
-}
