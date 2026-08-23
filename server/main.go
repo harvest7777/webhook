750ca27efe96f1d2c2a7b2193c865cd52f55f443
harvest7777 <333harvest.moon333@gmail.com>
2026-08-22 18:41:23 -0700

    simple http post

--- server/main.go
+++ server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hi")
+	url := "http://restapi3.apiary.io/notes"
+	fmt.Println("URL:>", url)
+
+	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 }
